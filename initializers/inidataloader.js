bf8f080248ea37515a576035e2736a4c1364e339
John Kokar <princedibloni4@gmail.com>
2024-04-20 13:09:19 -0700

    dreaa

--- initializers/inidataloader.js
+++ initializers/inidataloader.js
@@ -1,7 +1,24 @@
+function Logged_Checker() {
+    const token = sessionStorage.getItem('tirhaka');
+
+    if (token) {
+
+        return true;
+    } else {
+        return false;
+    }
+}
+
 async function initDataLoader() {
+    const homconnec = document.getElementById('homconnec');
+    if (Logged_Checker()) {
+        homconnec.innerText = "Mon Espace";
+        homconnec.setAttribute("href", "/dashboard");
+    }
+
+
     try {
-        const online = await requesttoBackend('GET', 'tirhakaservicegetting');
-        console.log(online);
+        //const online = await requesttoBackend('GET', 'tirhakaservicegetting');
 
     } catch (error) {
         console.log(error)
