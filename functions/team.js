620afce044a9c4fd3caa779233492672e7556faa
John Kokar <princedibloni4@gmail.com>
2024-04-20 19:08:39 -0700

    dreaa

---
+++ functions/team.js
@@ -0,0 +1,20 @@
+function Logged_Team() {
+    const token = sessionStorage.getItem('tirhaka');
+    const homconnec = document.getElementById('homconnec');
+
+    if (token) {
+        const splo = token.split("°");
+        if (thisiswhat(`${splo[2]}`) === "client") {
+            window.location.href = "/dashboard"
+
+        } else {
+            homconnec.innerText = "Mon Espace";
+            homconnec.setAttribute("href", "/dashboard");
+        }
+
+    } else {
+        window.location.href = "/dashboard"
+    }
+}
+
+Logged_Team()
